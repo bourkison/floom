7a9f1913c0a739650b35bd11f084d55796e06f90
bourkison <harrisonbourke97@gmail.com>
2022-09-14 23:01:04 +0100

    Built mongoose connection

---
+++ amplify/backend/function/teenderMongooseModels/lib/nodejs/models.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const mongooseConnect = require("./mongoose-connection");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: false,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+});
+
+const productSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  imageLink: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+});
+
+module.exports = () => {
+  const User = async (uri) => {
+    const connection = await mongooseConnect(uri);
+    const response = await connection.model("User", userSchema);
+    return response;
+  };
+
+  const Product = async (uri) => {
+    const connection = await mongooseConnect(uri);
+    const response = await connection.model("Product", productSchema);
+  };
+};
---
+++ amplify/backend/function/teenderMongooseModels/lib/nodejs/mongoose-connection.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+let conn = null;
+
+module.exports = () => {
+    if (conn === null) {
+        conn = await mongoose.createConnection({
+            useNewUrlParser: true,
+            bufferCommands: false,
+            serverSelectionTimeoutMS: 5000,
+            autoReconnect: true,
+            reconnectTries: Number.MAX_VALUE,
+            reconnectInterval: 1000,
+            keepAlive: 30000,
+            bufferMaxEntries: false
+        });
+    } else {
+        conn = await conn;
+    }
+
+    // Close the Mongoose connection, when receiving SIGINT
+    process.on('SIGINT', () => {
+        mongoose.connection.close(() => {
+            console.log('Force to close the MongoDB connection after SIGINT')
+            process.exit(0)
+        })
+    })
+
+    return conn;
+}
