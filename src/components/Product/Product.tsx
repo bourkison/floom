800123300bde1118f610358fb07498c6ef629878
bourkison <harrisonbourke97@gmail.com>
2022-09-16 20:41:14 +0100

    Added action text on swipe

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -20,12 +20,14 @@ const Product: React.FC<ProductComponentProps> = ({product, onSave}) => {
     const offsetX = useSharedValue(0);
     const offsetY = useSharedValue(0);
     const rotation = useSharedValue(0);
-    const opacity = useSharedValue(1);
+    const tileOpacity = useSharedValue(1);
+    const saveOpacity = useSharedValue(0);
+    const deleteOpacity = useSharedValue(0);
     const ctx = useSharedValue({x: 0, y: 0});
 
-    const rStyle = useAnimatedStyle(() => {
+    const rTileStyle = useAnimatedStyle(() => {
         return {
-            opacity: opacity.value,
+            opacity: tileOpacity.value,
             transform: [
                 {
                     translateX: offsetX.value,
@@ -37,6 +39,18 @@ const Product: React.FC<ProductComponentProps> = ({product, onSave}) => {
                     rotateZ: `${rotation.value}deg`,
                 },
             ],
+        };
+    });
+
+    const rSaveStyle = useAnimatedStyle(() => {
+        return {
+            opacity: saveOpacity.value,
+        };
+    });
+
+    const rDeleteStyle = useAnimatedStyle(() => {
+        return {
+            opacity: deleteOpacity.value,
         };
     });
 
@@ -52,58 +66,96 @@ const Product: React.FC<ProductComponentProps> = ({product, onSave}) => {
             offsetY.value = e.translationY + ctx.value.y;
 
             // Calculate rotation
-            const MAX_ROTATION = 15;
-            const ROTATION_WIDTH = 300;
+            const MAX_ROTATION = 10;
+            const ROTATION_WIDTH = 200;
+            const ACTION_VISIBILITY_THRESHOLD = 0.2;
 
-            let rot = (e.translationX / ROTATION_WIDTH) * MAX_ROTATION;
-            if (rot > ROTATION_WIDTH) {
-                rot = ROTATION_WIDTH;
-            } else if (rot < -ROTATION_WIDTH) {
-                rot = -ROTATION_WIDTH;
+            let percentageToAction = e.translationX / ROTATION_WIDTH;
+            if (percentageToAction > 1) {
+                percentageToAction = 1;
+            } else if (percentageToAction < -1) {
+                percentageToAction = -1;
             }
 
-            rotation.value = rot;
+            rotation.value = percentageToAction * MAX_ROTATION;
+
+            if (percentageToAction > ACTION_VISIBILITY_THRESHOLD) {
+                saveOpacity.value = percentageToAction;
+                deleteOpacity.value = 0;
+            } else if (percentageToAction < -ACTION_VISIBILITY_THRESHOLD) {
+                saveOpacity.value = 0;
+                deleteOpacity.value = -percentageToAction;
+            } else {
+                saveOpacity.value = 0;
+                deleteOpacity.value = 0;
+            }
         })
         .onFinalize(() => {
-            const ACTION_THRESHOLD = 200;
+            const ACTION_THRESHOLD = 150;
 
             if (offsetX.value > ACTION_THRESHOLD) {
                 // SAVE
-                opacity.value = withTiming(0, {}, runOnJS(onSave));
+                tileOpacity.value = withTiming(0, {}, runOnJS(onSave));
             } else if (offsetX.value < -ACTION_THRESHOLD) {
                 // DELETE
-                opacity.value = withTiming(0, {}, runOnJS(onSave));
+                tileOpacity.value = withTiming(0, {}, runOnJS(onSave));
             } else {
                 offsetX.value = withSpring(0);
                 offsetY.value = withSpring(0);
                 rotation.value = withSpring(0);
+                saveOpacity.value = withTiming(0);
+                deleteOpacity.value = withTiming(0);
             }
         });
 
     return (
-        <Animated.View style={[rStyle, styles.container]}>
+        <Animated.View style={[rTileStyle, styles.container]}>
             <GestureDetector gesture={panGesture}>
                 <View>
                     <ImageBackground
                         style={styles.image}
                         source={{uri: product.imageLink[0]}}>
-                        <View style={styles.gradientContainer}>
-                            <LinearGradient
-                                colors={['#00000000', '#000000']}
-                                style={styles.linearGradient}>
-                                <View style={styles.textContainer}>
-                                    <View style={{flex: 3}}>
-                                        <Text style={styles.titleText}>
-                                            {product.title}
-                                        </Text>
+                        <View style={styles.imageOverlayContainer}>
+                            <View style={styles.gradientContainer}>
+                                <LinearGradient
+                                    colors={['#00000000', '#000000']}
+                                    style={styles.linearGradient}>
+                                    <View style={styles.textContainer}>
+                                        <View style={{flex: 3}}>
+                                            <Text style={styles.titleText}>
+                                                {product.title}
+                                            </Text>
+                                        </View>
+                                        <View style={{flex: 1}}>
+                                            <Text style={styles.priceText}>
+                                                ${product.price}
+                                            </Text>
+                                        </View>
                                     </View>
-                                    <View style={{flex: 1}}>
-                                        <Text style={styles.priceText}>
-                                            ${product.price}
-                                        </Text>
-                                    </View>
-                                </View>
-                            </LinearGradient>
+                                </LinearGradient>
+                            </View>
+
+                            <View style={styles.actionContainer}>
+                                <Animated.View
+                                    style={[
+                                        rSaveStyle,
+                                        styles.saveTextContainer,
+                                    ]}>
+                                    <Text style={styles.saveText}>SAVE</Text>
+                                </Animated.View>
+                            </View>
+
+                            <View style={styles.actionContainer}>
+                                <Animated.View
+                                    style={[
+                                        rDeleteStyle,
+                                        styles.deleteTextContainer,
+                                    ]}>
+                                    <Text style={styles.deleteText}>
+                                        DELETE
+                                    </Text>
+                                </Animated.View>
+                            </View>
                         </View>
                     </ImageBackground>
                 </View>
@@ -125,6 +177,16 @@ const styles = StyleSheet.create({
     image: {
         width: 300,
         height: 300,
+    },
+    imageOverlayContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flex: 1,
+        justifyContent: 'center',
+        flexDirection: 'row',
     },
     gradientContainer: {
         position: 'absolute',
@@ -148,6 +210,40 @@ const styles = StyleSheet.create({
         padding: 10,
         flex: 1,
         flexDirection: 'row',
+    },
+    actionContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    saveTextContainer: {
+        borderColor: 'green',
+        borderWidth: 3,
+        borderRadius: 10,
+        padding: 5,
+        position: 'absolute',
+    },
+    saveText: {
+        color: 'green',
+        fontWeight: 'bold',
+        fontSize: 24,
+    },
+    deleteTextContainer: {
+        borderColor: 'red',
+        borderWidth: 3,
+        borderRadius: 10,
+        padding: 5,
+        position: 'absolute',
+    },
+    deleteText: {
+        color: 'red',
+        fontWeight: 'bold',
+        fontSize: 24,
     },
 });
 
