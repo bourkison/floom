9f51f2f7e6760ce54270cf196f98d79fd81a0a1c
bourkison <harrisonbourke97@gmail.com>
2022-10-15 13:56:39 +0100

    Updated action buttons.

--- src/screens/Swipe/Home.tsx
+++ src/screens/Swipe/Home.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ProductList from '@/components/Product/ProductList';
 import {View, StyleSheet, useWindowDimensions} from 'react-native';
-import ActionButton from '@/components/Utility/ActionButton';
 
 import {StackScreenProps} from '@react-navigation/stack';
 import {MainStackParamList} from '@/nav/Navigator';
 
 import {IMAGE_RATIO, IMAGE_PADDING} from '@/constants';
 import FilterDropdown from '@/components/Product/FilterDropdown';
+import AnimatedButton from '@/components/Utility/AnimatedButton';
+
+import {Feather, AntDesign, Ionicons} from '@expo/vector-icons';
+import {useAppDispatch} from '@/store/hooks';
+import {COMMENCE_ANIMATE} from '@/store/slices/product';
+
+const ACTION_BUTTON_SIZE = 50;
 
 const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
     const {width} = useWindowDimensions();
+    const dispatch = useAppDispatch();
 
     return (
         <View style={styles.flexOne}>
@@ -24,21 +31,45 @@ const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
                     <ProductList />
                 </View>
                 <View style={styles.buttonsContainer}>
-                    <ActionButton
-                        type="delete"
-                        radius={50}
-                        style={styles.button}
-                    />
-                    <ActionButton
-                        type="buy"
-                        radius={50}
-                        style={styles.button}
-                    />
-                    <ActionButton
-                        type="save"
-                        radius={50}
-                        style={styles.button}
-                    />
+                    <AnimatedButton
+                        style={styles.actionButton}
+                        onPress={() => {
+                            dispatch(COMMENCE_ANIMATE('delete'));
+                        }}>
+                        <View style={styles.button}>
+                            <Feather
+                                name="x"
+                                size={ACTION_BUTTON_SIZE / 2}
+                                color="#ce3b54"
+                            />
+                        </View>
+                    </AnimatedButton>
+                    <AnimatedButton
+                        style={styles.actionButton}
+                        onPress={() => {
+                            dispatch(COMMENCE_ANIMATE('buy'));
+                        }}>
+                        <View style={styles.button}>
+                            <AntDesign
+                                name="shoppingcart"
+                                size={ACTION_BUTTON_SIZE / 2}
+                                color="#264653"
+                            />
+                        </View>
+                    </AnimatedButton>
+                    <AnimatedButton
+                        style={styles.actionButton}
+                        onPress={() => {
+                            dispatch(COMMENCE_ANIMATE('save'));
+                        }}>
+                        <View style={styles.button}>
+                            <Ionicons
+                                name="heart"
+                                size={ACTION_BUTTON_SIZE / 2}
+                                color="#2a9d8f"
+                            />
+                        </View>
+                    </AnimatedButton>
                 </View>
             </View>
         </View>
@@ -62,7 +93,24 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
     },
     button: {
-        marginHorizontal: 10,
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    actionButton: {
+        shadowColor: '#1a1f25',
+        shadowOffset: {
+            height: 1,
+            width: 1,
+        },
+        shadowOpacity: 0.2,
+        backgroundColor: '#f3fcf0',
+        zIndex: 1,
+        borderRadius: ACTION_BUTTON_SIZE / 2,
+        width: ACTION_BUTTON_SIZE,
+        height: ACTION_BUTTON_SIZE,
+        marginHorizontal: 8,
     },
     flexOne: {flex: 1},
 });
