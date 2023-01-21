49c2ddfcbb2d04c2f018b7b553a9edcaefa15d78
bourkison <harrisonbourke97@gmail.com>
2023-01-21 01:39:24 +0000

    Update password added

---
+++ src/components/Options/UpdatePasswordWidget.tsx
@@ -0,0 +1,137 @@
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import AnimatedButton from '@/components/Utility/AnimatedButton';
+import Spinner from '@/components/Utility/Spinner';
+import TextInput from '@/components/Utility/TextInput';
+import {PALETTE} from '@/constants';
+import {Auth} from 'aws-amplify';
+
+const UpdatePasswordWidget = () => {
+    const [secureTextP, setSecureTextP] = useState(false);
+    const [secureTextNP, setSecureTextNP] = useState(false);
+    const [secureTextCNP, setSecureTextCNP] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confNewPassword, setConfNewPassword] = useState('');
+    const [isUpdatingPassword, setIsUpdatingPassword] = useState(false);
+
+    const updatePassword = async () => {
+        setIsUpdatingPassword(true);
+
+        try {
+            if (newPassword !== confNewPassword) {
+                throw new Error("Passwords don't match!");
+            }
+            const user = await Auth.currentAuthenticatedUser();
+            await Auth.changePassword(user, password, newPassword);
+        } catch (err) {
+            // TODO: Handle error.
+            console.error(err);
+        } finally {
+            setIsUpdatingPassword(false);
+        }
+    };
+
+    return (
+        <View>
+            <TextInput
+                placeholder="Current Password"
+                placeholderTextColor="#343E4B"
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect={false}
+                secureTextEntry={secureTextP}
+                editable={!isUpdatingPassword}
+                onFocus={() => {
+                    // Hacky way due to the below issue:
+                    // https://github.com/facebook/react-native/issues/21911
+                    setSecureTextP(true);
+                }}
+                style={styles.passwordInput}
+            />
+            <TextInput
+                placeholder="New Password"
+                placeholderTextColor="#343E4B"
+                onChangeText={setNewPassword}
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect={false}
+                secureTextEntry={secureTextNP}
+                editable={!isUpdatingPassword}
+                onFocus={() => {
+                    // Hacky way due to the below issue:
+                    // https://github.com/facebook/react-native/issues/21911
+                    setSecureTextNP(true);
+                }}
+                style={styles.passwordInput}
+            />
+            <TextInput
+                placeholder="Confirm New Password"
+                placeholderTextColor="#343E4B"
+                onChangeText={setConfNewPassword}
+                autoCapitalize="none"
+                autoComplete="off"
+                autoCorrect={false}
+                secureTextEntry={secureTextCNP}
+                editable={!isUpdatingPassword}
+                onFocus={() => {
+                    // Hacky way due to the below issue:
+                    // https://github.com/facebook/react-native/issues/21911
+                    setSecureTextCNP(true);
+                }}
+            />
+            <View style={styles.buttonContainer}>
+                <AnimatedButton
+                    style={styles.updatePasswordButton}
+                    textStyle={styles.updatePasswordButtonText}
+                    onPress={updatePassword}
+                    disabled={isUpdatingPassword}>
+                    {isUpdatingPassword ? (
+                        <Spinner
+                            diameter={14}
+                            spinnerWidth={2}
+                            backgroundColor="#1a1f25"
+                            spinnerColor="#f3fcfa"
+                        />
+                    ) : (
+                        'Update Password'
+                    )}
+                </AnimatedButton>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        padding: 10,
+    },
+    updatePasswordButton: {
+        padding: 7,
+        backgroundColor: PALETTE.neutral[8],
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 25,
+        width: '100%',
+        flexGrow: 0,
+        flexShrink: 0,
+        alignSelf: 'center',
+    },
+    updatePasswordButtonText: {
+        color: PALETTE.gray[1],
+        fontSize: 12,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        flexBasis: 14,
+        flexShrink: 0,
+        flexGrow: 0,
+    },
+    passwordInput: {
+        marginBottom: 10,
+    },
+});
+
+export default UpdatePasswordWidget;
--- src/screens/Options/Options.tsx
+++ src/screens/Options/Options.tsx
@@ -1,30 +1,14 @@
 import React, {useState} from 'react';
 import {Text, ScrollView, StyleSheet, View} from 'react-native';
 import TextInput from '@/components/Utility/TextInput';
-import Spinner from '@/components/Utility/Spinner';
 import SectionHeader from '@/components/Utility/SectionHeader';
 import {useAppSelector} from '@/store/hooks';
-import AnimatedButton from '@/components/Utility/AnimatedButton';
-import {PALETTE} from '@/constants';
+import UpdatePasswordWidget from '@/components/Options/UpdatePasswordWidget';
 import DeletedProductsWidget from '@/components/Options/DeletedProductsWidget';
 
 const Options = () => {
     const user = useAppSelector(state => state.user.docData);
     const [name, setName] = useState(user?.name || '');
-
-    const [secureTextP, setSecureTextP] = useState(false);
-    const [secureTextNP, setSecureTextNP] = useState(false);
-    const [secureTextCNP, setSecureTextCNP] = useState(false);
-
-    const [password, setPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confNewPassword, setConfNewPassword] = useState('');
-    const [isUpdatingPassword, setIsUpdatingPassword] = useState(false);
-
-    const updatePassword = async () => {
-        setIsUpdatingPassword(!isUpdatingPassword);
-        console.log('Update', password, newPassword, confNewPassword);
-    };
 
     if (!user) {
         return (
@@ -59,71 +43,7 @@ const Options = () => {
 
             <View style={styles.section}>
                 <SectionHeader>Password</SectionHeader>
-                <TextInput
-                    placeholder="Current Password"
-                    placeholderTextColor="#343E4B"
-                    onChangeText={setPassword}
-                    autoCapitalize="none"
-                    autoComplete="off"
-                    autoCorrect={false}
-                    secureTextEntry={secureTextP}
-                    editable={!isUpdatingPassword}
-                    onFocus={() => {
-                        // Hacky way due to the below issue:
-                        // https://github.com/facebook/react-native/issues/21911
-                        setSecureTextP(true);
-                    }}
-                    style={styles.passwordInput}
-                />
-                <TextInput
-                    placeholder="New Password"
-                    placeholderTextColor="#343E4B"
-                    onChangeText={setNewPassword}
-                    autoCapitalize="none"
-                    autoComplete="off"
-                    autoCorrect={false}
-                    secureTextEntry={secureTextNP}
-                    editable={!isUpdatingPassword}
-                    onFocus={() => {
-                        // Hacky way due to the below issue:
-                        // https://github.com/facebook/react-native/issues/21911
-                        setSecureTextNP(true);
-                    }}
-                    style={styles.passwordInput}
-                />
-                <TextInput
-                    placeholder="Confirm New Password"
-                    placeholderTextColor="#343E4B"
-                    onChangeText={setConfNewPassword}
-                    autoCapitalize="none"
-                    autoComplete="off"
-                    autoCorrect={false}
-                    secureTextEntry={secureTextCNP}
-                    editable={!isUpdatingPassword}
-                    onFocus={() => {
-                        // Hacky way due to the below issue:
-                        // https://github.com/facebook/react-native/issues/21911
-                        setSecureTextCNP(true);
-                    }}
-                />
-                <View style={styles.buttonContainer}>
-                    <AnimatedButton
-                        style={styles.updatePasswordButton}
-                        textStyle={styles.updatePasswordButtonText}
-                        onPress={updatePassword}
-                        disabled={isUpdatingPassword}>
-                        {isUpdatingPassword ? (
-                            <Spinner
-                                diameter={14}
-                                spinnerWidth={2}
-                                backgroundColor="#1a1f25"
-                                spinnerColor="#f3fcfa"
-                            />
-                        ) : (
-                            'Update Password'
-                        )}
-                    </AnimatedButton>
-                </View>
+                <UpdatePasswordWidget />
             </View>
         </ScrollView>
     );
@@ -141,33 +61,6 @@ const styles = StyleSheet.create({
         fontSize: 14,
         marginVertical: 5,
         fontWeight: 'bold',
-    },
-    buttonContainer: {
-        padding: 10,
-    },
-    updatePasswordButton: {
-        padding: 7,
-        backgroundColor: PALETTE.neutral[8],
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 25,
-        width: '100%',
-        flexGrow: 0,
-        flexShrink: 0,
-        alignSelf: 'center',
-    },
-    updatePasswordButtonText: {
-        color: PALETTE.gray[1],
-        fontSize: 12,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        flexBasis: 14,
-        flexShrink: 0,
-        flexGrow: 0,
-    },
-    passwordInput: {
-        marginBottom: 10,
     },
 });
 
