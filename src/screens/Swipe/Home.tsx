67f0162c0cce26ce577fbb65c706a49c77d45434
bourkison <harrisonbourke97@gmail.com>
2023-01-27 17:10:40 +0000

    Removed skia changes

--- src/screens/Swipe/Home.tsx
+++ src/screens/Swipe/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductList from '@/components/Product/ProductList';
-import {View, StyleSheet, useWindowDimensions, Text} from 'react-native';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 
 import {StackScreenProps} from '@react-navigation/stack';
 import {MainStackParamList} from '@/nav/Navigator';
@@ -10,8 +10,6 @@ import FilterDropdown from '@/components/Product/FilterDropdown';
 
 import ActionButton from '@/components/Utility/ActionButton';
 import {useAppSelector} from '@/store/hooks';
-import SkiaButton from '@/components/Utility/SkiaButton';
-import {useValue} from '@shopify/react-native-skia';
 
 const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
     const {width} = useWindowDimensions();
@@ -27,8 +25,6 @@ const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
         state => state.product.unsaved.products.length,
     );
 
-    const pressed = useValue(0);
-
     return (
         <View style={styles.flexOne}>
             <FilterDropdown obj="unsaved" />
@@ -41,7 +37,7 @@ const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
                     <ProductList />
                 </View>
                 {/* Don't render action buttons if no products found. */}
-                {/* <View
+                <View
                     style={[
                         styles.buttonsContainer,
                         !isLoading &&
@@ -54,20 +50,6 @@ const Home = ({}: StackScreenProps<MainStackParamList, 'Home'>) => {
                     <ActionButton type="delete" />
                     <ActionButton type="buy" />
                     <ActionButton type="save" />
-                </View> */}
-                <View
-                    style={{
-                        flex: 1,
-                        width: '100%',
-                    }}>
-                    <SkiaButton
-                        x={0}
-                        y={0}
-                        width={100}
-                        height={100}
-                        pressed={pressed}>
-                        <View />
-                    </SkiaButton>
                 </View>
             </View>
         </View>
